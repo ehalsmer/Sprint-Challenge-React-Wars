58efc4cc1bd939c82d288f184643e5f0afc828a7
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-19 11:48:46 -0600

    Cards displaying with titles and brief character description. Using mock API from Henry because SWAPI went down

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { Container, Header, Input, Item, Divider, Card } from "semantic-ui-react";
+import PersonCard from './PersonCard';
+import {Grid} from 'semantic-ui-react';
+import "semantic-ui-css/semantic.min.css";
 import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState([]);
+
+useEffect(()=>{
+  // axios.get('https://swapi.co/api/people/?page=1')
+  axios.get('https://henry-mock-swapi.herokuapp.com/api')
+  .then(data=>{
+    const peopleArray = data.data.results;
+  //   console.log(peopleArray);
+    setPeople(peopleArray);
+    // peopleArray.forEach((person)=>{
+    //   console.log(person.name)
+    // })
+  // return peopleArray;
+  })
+}, [])
 
   return (
-    <div className="App">
+    <Container style={{backgroundImage: "url('./sw-bg.jpg')"}} className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+      <Card.Group centered itemsPerRow={3}>
+      {people.map((person, index)=>{
+        
+        return <PersonCard key = {person.name} name={person.name} birthYear={person.birth_year} vehicles={person.vehicles} eyeColor={person.eye_color} hairColor={person.hair_color} imgSrc={`./img/${index}.jpg`}></PersonCard>
+      })}
+      </Card.Group>
+    </Container>
   );
 }
 
